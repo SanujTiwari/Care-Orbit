00ebcf7b5079cecc91e7e1c287c20d19337211a8
SanujTiwari <sanujvirat@gmail.com>
2026-02-19 23:30:52 +0530

    fix wildcard route error

--- backend/index.js
+++ backend/index.js
@@ -23,9 +23,10 @@ app.use("/api/appointments", require("./routes/appointmentRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-  });
+  app.get('/*', (req, res) => {
+  res.send('API Running');
+});
+
 }
 
 const PORT = process.env.PORT || 5000;
