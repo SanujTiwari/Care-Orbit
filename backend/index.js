c7ad67b5f6fa310dc38f7736a17f0288f3a592a2
SanujTiwari <sanujvirat@gmail.com>
2026-02-19 20:27:29 +0530

    Fix express duplicate and serve frontend

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,11 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
+
 const connectDB = require("./config/db");
 
 dotenv.config();
-
-console.log("JWT Secret:", process.env.JWT_SECRET);
 
 connectDB();
 
@@ -20,20 +20,16 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/appointments", require("./routes/appointmentRoutes"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
-import path from "path";
-import express from "express";
-
-const app = express();
-
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   });
 }
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
