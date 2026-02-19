6656557b75b25baa4188f39cb8919a0444964764
Sanuj Tiwari <145697474+SanujTiwari@users.noreply.github.com>
2026-02-19 20:06:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,3 +23,17 @@ app.use("/api/appointments", require("./routes/appointmentRoutes"));
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+import path from "path";
+import express from "express";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
